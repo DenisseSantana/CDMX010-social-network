718081cd1ccc955d09d0ca2c135bedb5267a3ad9
Denisse <ledennii@gmail.com>
2021-04-04 19:55:40 -0500

    Likes

--- test/home.spec.js
+++ test/home.spec.js
@@ -1,10 +1,10 @@
 import {toViewHome} from '../src/components/home.js'
 
-// describe('toViewHome', () => {
-//     it('debería ser una función', () => {
-//       expect(typeof toViewHome).toBe('function');
-//     });
-//   });
+describe('toViewHome', () => {
+    it('debería ser una función', () => {
+      expect(typeof toViewHome).toBe('function');
+    });
+  });
 
 // describe('Test home', ()=>{
 //     beforeEach(()=>{
@@ -21,18 +21,18 @@ import {toViewHome} from '../src/components/home.js'
 //     expect(container.innerHTML).toMatchSnapshot();
 //   });  
 // });
-describe('Test home', ()=>{
-    beforeEach(()=>{
-        document.body.innerHTML=`<div id='root'></div>`;
-    });
-  test('Button event', async ()=>{
-      const container = document.getElementById('root');
-      const post = [
-          {author:'Fulanito'},
-      ];
-      const getPost = jest.fn().mockImplemantation(() => Promise.resolve(post));
-      const firebase = {getPost};
-    await toViewHome (container,firebase);
-    expect(container.innerHTML).toMatchSnapshot();
-  });  
-});
+// describe('Test home', ()=>{
+//     beforeEach(()=>{
+//         document.body.innerHTML=`<div id='root'></div>`;
+//     });
+//   test('Button event', async ()=>{
+//       const container = document.getElementById('root');
+//       const post = [
+//           {author:'Fulanito'},
+//       ];
+//       const getPost = jest.fn().mockImplemantation(() => Promise.resolve(post));
+//       const firebase = {getPost};
+//     await toViewHome (container,firebase);
+//     expect(container.innerHTML).toMatchSnapshot();
+//   });  
+// });
